80d0a94977c7c94727f07d1ccade1ff74b029330
NiLuJe <ninuje@gmail.com>
2018-11-21 20:23:50 +0100

    Add a touch  of setuptools, to hopefully make cross-compilation less painful...

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+from setuptools import setup, find_packages
+setup(
+	name="FBInk",
+	version="1.0",
+	packages=find_packages(),
+	scripts=['hello.py'],
+
+	# We kinda need cffi ;).
+	setup_requires=["cffi>=1.0.0"],
+	cffi_modules=["fbink_build.py:ffibuilder"],
+	install_requires=["cffi>=1.0.0"],
+
+	# Metadata
+	author="NiLuJe",
+	author_email="ninuje@gmail.com",
+	description="cFFI bindings for FBInk",
+	license="AGPLv3+",
+	keywords="fbink ffi cffi",
+	url="https://github.com/NiLuJe/py-fbink/",
+)
